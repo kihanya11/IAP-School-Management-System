94d9fd70c12edb5d50abf9ec72e137a935d46cf4
kihanya11 <kihanya01@gmail.com>
2022-11-08 12:10:41 +0300

    jui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address or Username') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address / Username') }}</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="username" class="form-control @error('username') is-invalid @enderror  @error('email') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
