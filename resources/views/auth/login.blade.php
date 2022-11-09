162af536025973fdd2e72d70a5c38628e9e5ac38
humphrey <humphrey.dulo@strathmore.edu>
2022-11-09 09:01:51 +0300

    {{ __('Forgot Your Password:') }}

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,7 +65,7 @@
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Forgot Your Password:') }}
                                     </a>
                                 @endif
                             </div>
