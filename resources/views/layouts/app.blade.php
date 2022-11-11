d8aa22728955fc9fc98712a0f603cd984e0dd444
kihanya11 <kihanya01@gmail.com>
2022-11-11 10:40:55 +0300

    homepage design complete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,19 +9,26 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <!-- Css -->
+    <link rel = "stylesheet" href= "{{ URL::asset('css/style.css')}}">
+
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand " id = "navbar__logo" href="{{ url('/') }}">
+                    <!--{{ config('app.name', 'Laravel') }}-->
+                    MiddleView
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
